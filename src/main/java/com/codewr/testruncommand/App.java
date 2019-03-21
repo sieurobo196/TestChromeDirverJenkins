ad00444ca21d1fd10d20a6ae9c76242af76be0f1
Sieurobo196 <xuancu2006@gmail.com>
2019-03-21 15:22:54 +0700

    #test build

--- src/main/java/com/codewr/testruncommand/App.java
+++ src/main/java/com/codewr/testruncommand/App.java
@@ -15,28 +15,17 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 public class App {
 
     public static void main(String[] args) {
-        try {
-            String chromeDriverPath = "/usr/bin/chromedriver";
-            System.setProperty("webdriver.chrome.driver", chromeDriverPath);
-            WebDriver driver = new ChromeDriver();
-//            ChromeOptions chromeOptions = new ChromeOptions();
-//            chromeOptions.addArguments("--headless");
-//            chromeOptions.addArguments("--no-sandbox");
-//
-//            WebDriver driver = new ChromeDriver(chromeOptions);
-//            WebDriver driver = new ChromeDriver(chromeOptions);
-//            WebDriver driver = new ChromeDriver(cap);
-            //Launch the google.com Website
-            driver.get("http://google.com");
-//            driver.get("http://www.mailinator.com/");
-//            driver.findElement(By.xpath("//input[@id='inboxfield']")).sendKeys("cutx");
-//            driver.findElement(By.xpath("//button[@class='btn btn-default']")).click();
-            //Wait for 5 Sec
-            Thread.sleep(10000);
-            // Close the driver
-            driver.close();
-        } catch (InterruptedException ex) {
-            Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+        int count = 0;
+        while (true) {
+            try {
+                System.out.println("Running...." + ++count);
+                Thread.sleep(1000);
+                if (count == 20) {
+                    break;
+                }
+            } catch (InterruptedException ex) {
+                System.out.println("Systemctl " + ex);
+            }
         }
     }
 }
